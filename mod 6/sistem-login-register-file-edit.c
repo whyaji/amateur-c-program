80cbeb90d103cd6ccc71613c9e0229718d15d15c
whyaji <wahyupatriaji@gmail.com>
2020-12-31 16:28:11 +0700

    mod 66

--- mod 6/sistem-login-register-file-edit.c
+++ mod 6/sistem-login-register-file-edit.c
@@ -2,16 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 
-int fUrutan, aUrutan;
+int fUrutan, aUrutan, nonDuplikat;
 char fUser[20], fPass[20], fKode[20], aUser[20], aPass[20], aKode[20];
+char kosong[] = "--";
 
-void mainUser();
 void registrasi();
 int inputUsername(char iUser[20]);
 int inputPassword(char iPass[20]);
 void listData();
 void login();
+void mainUser();
 void akunAnda();
+void editAkun();
+void deleteAkun();
 void delEdiAkun(char bUser[20], char bPass[20], char bKode[20]);
 void delete ();
 
@@ -56,90 +59,12 @@ int main()
     return 0;
 }
 
-void mainUser()
-{
-    int menu, j = 1;
-    char cM[64];
-    int fAUUrutan;
-    char fAUser[20], fAPass[20], fAKode[20];
-    char kosong[3];
-    strcpy(kosong, "--");
-
-    fDList = fopen("listdata.txt", "r");
-    while (fscanf(fDList, "%d %s %s %s\n", &fUrutan, fUser, fPass, fKode) != EOF)
-    {
-        j++;
-    }
-    fclose(fDList);
-
-    fDList = fopen("listdata.txt", "r");
-    for (int i = 1; i <= j; i++)
-    {
-        fscanf(fDList, "%d %s %s %s\n", &fAUUrutan, fAUser, fAPass, fAKode);
-        if (!((strcmp(kosong, fAKode) == 0 || strcmp(kosong, fAUser) == 0) && strcmp(kosong, fAPass) == 0))
-        {
-            break;
-        }
-    }
-    fclose(fDList);
-
-    if ((strcmp(fAKode, aUser) == 0 || strcmp(fAUser, aUser) == 0) && strcmp(fAPass, aPass) == 0)
-    {
-        printf("== Halo %s (admin) ==\n 1. Akun anda\n 2. Logut\n 3. Tampilkan semua data\n 4. Hapus semua data\n", aUser);
-        printf("PILIH MENU : ");
-        gets(cM);
-        if (*cM < '1' || *cM > '4' || strlen(cM) > 1)
-        {
-            printf("Menu tidak ada\n\n");
-            mainUser();
-        }
-        else
-        {
-            sscanf(cM, "%d", &menu);
-        }
-    }
-    else
-    {
-        printf("== Halo %s ==\n 1. Akun anda\n 2. Logut\n 3. Tampilkan semua data\n", aUser);
-        printf("PILIH MENU : ");
-        gets(cM);
-        if (*cM < '1' || *cM > '3' || strlen(cM) > 1)
-        {
-            printf("Menu tidak ada\n\n");
-            mainUser();
-        }
-        else
-        {
-            sscanf(cM, "%d", &menu);
-        }
-    }
-
-    switch (menu)
-    {
-    case 1:
-        akunAnda();
-        mainUser();
-
-    case 2:
-        printf("Anda telah logout\n\n");
-        main();
-
-    case 3:
-        listData();
-        mainUser();
-
-    case 4:
-        delete();
-        main();
-    }
-}
-
 void registrasi()
 {
-    int j = 1;
+    int j = 1, pengurangan = 1;
     char rUser[20], rPass[20], rKode[20], tigaH[3];
 
-    printf("==REGISTRASI==\n");
+    printf("\n==REGISTRASI==\n");
     printf("Input Username : ");
     gets(rUser);
     printf("Input Password : ");
@@ -151,6 +76,10 @@ void registrasi()
         while (fscanf(fDList, "%d %s %s %s\n", &fUrutan, fUser, fPass, fKode) != EOF)
         {
             j++;
+            if (strcmp(kosong, fUser) == 0)
+            {
+                j--;
+            }
         }
         strcpy(tigaH, rUser);
         sprintf(rKode, "%.3s-%.4d", strupr(tigaH), j);
@@ -195,16 +124,20 @@ int inputUsername(char iUser[20])
         }
 
         int duplikat = 0;
-        fDList = fopen("listdata.txt", "r");
-        while (fscanf(fDList, "%d %s %s %s\n", &fUrutan, fUser, fPass, fKode) != EOF)
+        if (nonDuplikat == 0)
         {
-            if (strcmp(fUser, iUser) == 0)
+            fDList = fopen("listdata.txt", "r");
+            while (fscanf(fDList, "%d %s %s %s\n", &fUrutan, fUser, fPass, fKode) != EOF)
             {
-                duplikat++;
-                break;
+                if (strcmp(fUser, iUser) == 0)
+                {
+                    duplikat++;
+                    break;
+                }
             }
+            fclose(fDList);
         }
-        fclose(fDList);
+
         if (duplikat != 0)
         {
             printf("Username sudah terpakai, Tidak boleh ada duplikasi username\n");
@@ -274,9 +207,10 @@ int inputPassword(char iPass[20])
 
 void listData()
 {
-    fDList = fopen("listdata.txt", "r");
-    printf("== LIST DATA ==\n");
+    int j = 1;
 
+    printf("\n== LIST DATA ==\n");
+    fDList = fopen("listdata.txt", "r");
     if (!fDList)
     {
         printf("Belum ada data teregister\n\n");
@@ -285,7 +219,11 @@ void listData()
 
     while (fscanf(fDList, "%d %s %s %s\n", &fUrutan, fUser, fPass, fKode) != EOF)
     {
-        printf("== %d ==\nUsername : %s\nKode User : %s\n", fUrutan, fUser, fKode);
+        if (strcmp(kosong, fUser) != 0)
+        {
+            printf("== %d ==\nUsername : %s\nKode User : %s\n", j, fUser, fKode);
+            j++;
+        }
     }
     printf("\n");
     fclose(fDList);
@@ -295,10 +233,8 @@ void login()
 {
     char lUser[20], lPass[20];
     int sukses = 0;
-    char kosong[3];
-    strcpy(kosong, "--");
 
-    printf("==LOGIN==\n");
+    printf("\n==LOGIN==\n");
 
     fDList = fopen("listdata.txt", "r");
 
@@ -318,7 +254,6 @@ void login()
         printf("User atau Password anda salah!\n\n");
         main();
     }
-    
 
     while (fscanf(fDList, "%d %s %s %s\n", &fUrutan, fUser, fPass, fKode) != EOF)
     {
@@ -336,10 +271,12 @@ void login()
         printf("== Data anda ==\n");
         printf("Username : %s\n", fUser);
         printf("Kode User : %s\n\n", fKode);
+
         strcpy(aUser, fUser);
         strcpy(aPass, fPass);
         strcpy(aKode, fKode);
         aUrutan = fUrutan;
+
         mainUser();
     }
     else
@@ -348,15 +285,85 @@ void login()
     }
 }
 
+void mainUser()
+{
+    int menu, j = 1;
+    char cM[64];
+    int fAUrutan;
+    char fAUser[20], fAPass[20], fAKode[20];
+
+    fDList = fopen("listdata.txt", "r");
+    while (fscanf(fDList, "%d %s %s %s\n", &fUrutan, fUser, fPass, fKode) != EOF)
+    {
+        j++;
+    }
+    fclose(fDList);
+
+    fDList = fopen("listdata.txt", "r");
+    for (int i = 1; i <= j; i++)
+    {
+        fscanf(fDList, "%d %s %s %s\n", &fAUrutan, fAUser, fAPass, fAKode);
+        if (strcmp(kosong, fAUser) != 0)
+        {
+            break;
+        }
+    }
+    fclose(fDList);
+
+    if ((strcmp(fAKode, aUser) == 0 || strcmp(fAUser, aUser) == 0) && strcmp(fAPass, aPass) == 0)
+    {
+        printf("== Halo %s (admin) ==\n 1. Akun anda\n 2. Logut\n 3. Tampilkan semua data\n 4. Hapus semua data\n", aUser);
+        printf("PILIH MENU : ");
+        gets(cM);
+        if (*cM < '1' || *cM > '4' || strlen(cM) > 1)
+        {
+            printf("Menu tidak ada\n\n");
+            mainUser();
+        }
+        else
+        {
+            sscanf(cM, "%d", &menu);
+        }
+    }
+    else
+    {
+        printf("== Halo %s ==\n 1. Akun anda\n 2. Logut\n 3. Tampilkan semua data\n", aUser);
+        printf("PILIH MENU : ");
+        gets(cM);
+        if (*cM < '1' || *cM > '3' || strlen(cM) > 1)
+        {
+            printf("Menu tidak ada\n\n");
+            mainUser();
+        }
+        else
+        {
+            sscanf(cM, "%d", &menu);
+        }
+    }
+
+    switch (menu)
+    {
+    case 1:
+        akunAnda();
+        mainUser();
+
+    case 2:
+        printf("Anda telah logout\n\n");
+        main();
+
+    case 3:
+        listData();
+        mainUser();
+
+    case 4:
+        delete ();
+        main();
+    }
+}
+
 void akunAnda()
 {
-    char bUser[20], bPass[20], bKode[20], tigaH[3];
-    char kosong[20] = "";
-    char bKosong[3];
-
-    strcpy(bKosong, "--");
-akun:
-    printf("== Akun Anda ==\n");
+    printf("\n== Akun Anda ==\n");
     printf("Username : %s\n", aUser);
     printf("Kode User : %s\n", aKode);
     printf("Password : ");
@@ -366,9 +373,9 @@ akun:
         printf("*");
     }
     printf("\n");
-    
-    int menu, jawab;
-    char cM[64], yakin[64], kPass[20];
+
+    int menu;
+    char cM[64];
 
     printf("Pilih\n 1. Edit akun anda\n 2. Hapus akun anda\n 3. Kembali\n");
     printf("PILIH MENU : ");
@@ -376,7 +383,7 @@ akun:
     if (*cM < '1' || *cM > '3' || strlen(cM) > 1)
     {
         printf("Pilihan tidak ada\n\n");
-        goto akun;
+        akunAnda();
     }
     else
     {
@@ -386,68 +393,100 @@ akun:
     switch (menu)
     {
     case 1:
-        printf("Masukan password anda\n");
-        gets(kPass);
-        if (strcmp(kPass, aPass) == 0)
-        {
-            printf("Masukan username dan password baru\n");
-            printf("Kosongkan bagian yang tidak diganti\n");
-            printf("Input username : ");
-            gets(bUser);
-            printf("Input password : ");
-            gets(bPass);
-
-            if (strcmp(bUser, kosong) == 0)
-            {
-                strcpy(bUser, aUser);
-            }
-            if (strcmp(bPass, kosong) == 0)
-            {
-                strcpy(bPass, aPass);
-            }
-
-            if (inputUsername(bUser) + inputPassword(bPass) == 2)
-            {
-                strcpy(tigaH, bUser);
-                sprintf(bKode, "%.3s-%.4d", strupr(tigaH), aUrutan);
-                delEdiAkun(bUser, bPass, bKode);
-                printf("Akun anda telah diperbarui\n\n");
-                goto akun;
-            }
-            else
-            {
-                printf("\nAkun anda gagal diperbarui\n\n");
-                goto akun;
-            }
-        }
-        else
-        {
-            printf("Password anda salah\n\n");
-            goto akun;
-        }
+        editAkun();
+        akunAnda();
 
     case 2:
-        printf("\nApakah anda yakin ingin menghapus akun ini? jika anda yakin\n");
-        printf("Masukan password anda\n");
-        gets(kPass);
-        if (strcmp(kPass, aPass) == 0)
-        {
-            strcpy(bUser, bKosong);
-            strcpy(bPass, bKosong);
-            strcpy(bKode, bKosong);
-            delEdiAkun(bUser, bPass, bKode);
-            printf("Akun anda telah dihapus\n\n");
-            main();
-        }
-        else
-        {
-            printf("Password anda salah\n\n");
-            goto akun;
-        }
+        deleteAkun();
+        akunAnda();
 
     case 3:
         printf("Kembali\n\n");
         mainUser();
+    }
+}
+
+void editAkun()
+{
+    char bUser[20], bPass[20], bKode[20], tigaH[3], kPass[20];
+    char ksng[20] = "";
+
+    printf("Masukan password anda\n");
+    gets(kPass);
+    if (strcmp(kPass, aPass) == 0)
+    {
+        printf("Masukan username dan password baru\n");
+        printf("Kosongkan bagian yang tidak diganti\n");
+        printf("Input username : ");
+        gets(bUser);
+        printf("Input password : ");
+        gets(bPass);
+        nonDuplikat = 0;
+
+        if (strcmp(bUser, ksng) == 0)
+        {
+            strcpy(bUser, aUser);
+            nonDuplikat++;
+        }
+        if (strcmp(bPass, ksng) == 0)
+        {
+            strcpy(bPass, aPass);
+        }
+
+        if (inputUsername(bUser) + inputPassword(bPass) == 2)
+        {
+            strcpy(tigaH, bUser);
+            sprintf(bKode, "%.3s-%.4d", strupr(tigaH), aUrutan);
+            delEdiAkun(bUser, bPass, bKode);
+            printf("\nAkun anda telah diperbarui\n");
+        }
+        else
+        {
+            printf("\nAkun anda gagal diperbarui\n");
+        }
+    }
+    else
+    {
+        printf("\nPassword anda salah\n");
+    }
+}
+
+void deleteAkun()
+{
+    char bUser[20], bPass[20], bKode[20], kPass[20];
+    int j = 0;
+
+    printf("\nApakah anda yakin ingin menghapus akun ini? jika anda yakin\n");
+    printf("Masukan password anda\n");
+    gets(kPass);
+    if (strcmp(kPass, aPass) == 0)
+    {
+        strcpy(bUser, kosong);
+        strcpy(bPass, kosong);
+        strcpy(bKode, kosong);
+        delEdiAkun(bUser, bPass, bKode);
+        printf("\nAkun anda telah dihapus\n\n");
+
+        fDList = fopen("listdata.txt", "r");
+        while (fscanf(fDList, "%d %s %s %s\n", &fUrutan, fUser, fPass, fKode) != EOF)
+        {
+            if (strcmp(kosong, fUser) != 0)
+            {
+                j++;
+            }
+        }
+        fclose(fDList);
+        if (j == 0)
+        {
+            remove("listdata.txt");
+            printf("\nDan semua data telah dihapus\n\n");
+        }
+        
+        main();
+    }
+    else
+    {
+        printf("\nPassword anda salah\n");
     }
 }
 
@@ -460,7 +499,10 @@ void delEdiAkun(char bUser[20], char bPass[20], char bKode[20])
         fscanf(fDList, "%d %s %s %s\n", &fUrutan, fUser, fPass, fKode);
         if (fUrutan == aUrutan)
         {
-            break;
+            if (strcmp(kosong, fUser) != 0)
+            {
+                break;
+            }
         }
         fprintf(fDTemp, "%d %s %s %s\n", fUrutan, fUser, fPass, fKode);
     }
@@ -470,7 +512,6 @@ void delEdiAkun(char bUser[20], char bPass[20], char bKode[20])
     fDList = fopen("listdata.txt", "r");
     fDTemp = fopen("lotemp.txt", "a+");
     fprintf(fDTemp, "%d %s %s %s\n", aUrutan, bUser, bPass, bKode);
-
     while (!feof(fDList))
     {
         fscanf(fDList, "%d %s %s %s\n", &fUrutan, fUser, fPass, fKode);
@@ -480,7 +521,7 @@ void delEdiAkun(char bUser[20], char bPass[20], char bKode[20])
             if (fUrutan == fUrutan - 1)
             {
                 break;
-            }   
+            }
         }
     }
     fclose(fDList);
@@ -496,14 +537,19 @@ void delEdiAkun(char bUser[20], char bPass[20], char bKode[20])
 
 void delete ()
 {
-    fDList = fopen("listdata.txt", "r");
-    if (!fDList)
+    char kPass[20];
+    printf("\nApakah anda yakin ingin menghapus semua data user? jika anda yakin\n");
+    printf("Masukan password anda\n");
+    gets(kPass);
+    if (strcmp(kPass, aPass) == 0)
     {
-        printf("Belum ada data teregister\n\n");
+        remove("listdata.txt");
+        printf("\nSemua data telah dihapus\n\n");
         main();
     }
-    fclose(fDList);
-
-    remove("listdata.txt");
-    printf("Data telah dihapus\n\n");
+    else
+    {
+        printf("\nPassword anda salah\n\n");
+        mainUser();
+    }
 }
